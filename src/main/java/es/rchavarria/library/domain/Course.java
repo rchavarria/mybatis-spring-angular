0a45c4eb988da0d4338309ca2c8a651d49f3b571
rchavarria <txingo@gmail.com>
2013-12-06 19:06:52 +0100

    simplify Course domain class

--- src/main/java/es/rchavarria/library/domain/Course.java
+++ src/main/java/es/rchavarria/library/domain/Course.java
@@ -3,10 +3,6 @@ package es.rchavarria.library.domain;
 public class Course {
 
     private String title;
-    private String teacher;
-    private String level;
-    private float hoursLong;
-    private boolean active;
     
     public String getTitle() {
         return title;
@@ -14,32 +10,5 @@ public class Course {
     public void setTitle(String title) {
         this.title = title;
     }
-    
-    public String getTeacher() {
-        return teacher;
-    }
-    public void setTeacher(String teacher) {
-        this.teacher = teacher;
-    }
-    
-    public String getLevel() {
-        return level;
-    }
-    public void setLevel(String level) {
-        this.level = level;
-    }
-    
-    public float getHoursLong() {
-        return hoursLong;
-    }
-    public void setHoursLong(float hoursLong) {
-        this.hoursLong = hoursLong;
-    }
-    
-    public boolean isActive() {
-        return active;
-    }
-    public void setActive(boolean active) {
-        this.active = active;
-    }
+
 }
