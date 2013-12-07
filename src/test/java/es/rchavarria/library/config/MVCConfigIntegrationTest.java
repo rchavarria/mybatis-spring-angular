a16e5d01885b2afc730eb9a49f5a55101071915e
rchavarria <txingo@gmail.com>
2013-12-07 19:55:35 +0100

    create a new test and its needed implementation to configure Spring MVC controllers to respond to HTTP requests

---
+++ src/test/java/es/rchavarria/library/config/MVCConfigIntegrationTest.java
@@ -0,0 +1,43 @@
+package es.rchavarria.library.config;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {CoreConfig.class, MVCConfig.class})
+public class MVCConfigIntegrationTest {
+
+  @Autowired
+  WebApplicationContext webApplicationContext;
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setup() {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+  }
+
+  @Test
+  public void addANewOrderToTheSystem() throws Exception  {
+    this.mockMvc.perform(get("/library/courses")
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].title").value("Title 1"));
+  }
+}
