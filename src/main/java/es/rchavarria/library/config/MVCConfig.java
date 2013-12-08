9f1916776829f57a9c77aee4105e90941e4a5aa9
rchavarria <txingo@gmail.com>
2013-12-09 00:20:20 +0100

    enable default servlet to handle static files

--- src/main/java/es/rchavarria/library/config/MVCConfig.java
+++ src/main/java/es/rchavarria/library/config/MVCConfig.java
@@ -2,9 +2,20 @@ package es.rchavarria.library.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"es.rchavarria.library.controller"})
-public class MVCConfig {}
+@ComponentScan(basePackages = { "es.rchavarria.library.controller" })
+public class MVCConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * Enable default servlet handler to serve static files under src/main/webapp
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
