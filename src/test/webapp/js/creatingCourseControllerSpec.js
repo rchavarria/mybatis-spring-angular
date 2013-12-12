01013471c5d1061def444de9da5a914178cfe882
rchavarria <txingo@gmail.com>
2013-12-12 14:31:07 +0100

    add client test to check CreateCourseCtrl initialization

---
+++ src/test/webapp/js/creatingCourseControllerSpec.js
@@ -0,0 +1,58 @@
+/*global angular, describe, beforeEach, it, expect */
+describe("Creating course controller", function () {
+    "use strict";
+    
+    var scope, ctrl, $httpBackend, returnedTeachers, returnedLevels;
+    
+    // adds a new Jasmine matcher, toEqualData, to check only data in 
+    //object and ignore functions
+    beforeEach(function () {
+        this.addMatchers({
+            toEqualData: function(expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+    });
+    
+    // data fixture
+    returnedTeachers = function () {
+        return [
+            { idTeacher: 0, name: "Teacher 1" },
+            { idTeacher: 1, name: "Teacher 2" },
+            { idTeacher: 2, name: "Teacher 3" }
+        ];
+    };
+    
+    returnedLevels = function () {
+        return [
+            { level: "BASIC" },
+            { level: "INTERMEDIATE" },
+            { level: "ADVANCED" }
+        ];
+    };
+    
+    // load the application module and its dependencies
+    beforeEach(module("librarian"));
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET("teachers").respond(returnedTeachers());
+        $httpBackend.expectGET("levels").respond(returnedLevels());
+        
+        scope = $rootScope.$new();
+        ctrl = $controller("CreateCourseCtrl", { $scope: scope });
+    }));
+    
+    it("invokes 'teachers' to get all of them from the server", function () {
+        $httpBackend.flush();
+        expect(scope.teachers.length).toBe(3);
+        expect(scope.teachers).toEqualData(returnedTeachers());
+    });
+    
+    it("invokes 'levels' to get all of them from the server", function () {
+        $httpBackend.flush();
+        expect(scope.levels.length).toBe(3);
+        expect(scope.levels).toEqualData(returnedLevels());
+    });
+
+});
